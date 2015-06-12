d20fee36d0e36736f9307843564ed866d6be305e
Jessica Engel <Jessicaepdx@gmail.com>
2015-06-12 10:09:45 -0700

    will list author and description on individual question page

--- routes/question.js
+++ routes/question.js
@@ -1,6 +1,5 @@
-MessageBoard.AllQuestionRoute = Ember.Route.extend({
-  model: function(){
-    return this.store.find('question');
+MessageBoard.QuestionRoute = Ember.Route.extend({
+  model: function(params) {
+    return this.store.find('question', params.question_id);
   }
-
 });
