280e09bef071dcdb7ffdf9a3efecca2334d0f76e
Jessica Engel <Jessicaepdx@gmail.com>
2015-06-12 08:56:53 -0700

    have navbar and header, initial files setup

---
+++ router.js
@@ -0,0 +1,11 @@
+MessageBoard.Router.map(function() {
+  this.resource('publicLots', {path:'/'});
+  this.resource('publicItems');
+  this.resource('lots');
+  this.resource('add-lot');
+  this.resource('lot', {path:'lots/:lot_id'});
+  this.resource('publicLot', {path:'publiclots/:lot_id'});
+  this.resource('item', {path:'items/:item_id'});
+  this.resource('publicItem', {path:'publicItems/:item_id'});
+
+});
