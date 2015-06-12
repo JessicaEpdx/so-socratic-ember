2d70697ebe99f87c6616c95220c67217c0b59714
Jessica Engel <Jessicaepdx@gmail.com>
2015-06-12 16:51:27 -0700

    all styled and organized replies on question  page

--- controllers/edit-question.js
+++ controllers/edit-question.js
@@ -7,8 +7,11 @@ MessageBoard.EditQuestionController = Ember.ObjectController.extend({
     },
     editQuestion: function() {
       var question = this.get('model');
+      var id = this.get('model.id')
       question.save();
       this.set('isUpdating', false);
+      this.transitionToRoute('question', id);
+
     },
     delete: function() {
       if(confirm('Are you sure?')) {
