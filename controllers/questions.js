6bdf2812010b166b15c11e15f2ff87294845e8d7
Jessica Engel <Jessicaepdx@gmail.com>
2015-06-12 12:59:17 -0700

    all buttons on home page will toggle between showing and hiding for add question form and list of questions

--- controllers/questions.js
+++ controllers/questions.js
@@ -1,8 +1,18 @@
 MessageBoard.QuestionsController = Ember.ArrayController.extend({
   isQuestioning: false,
+  isViewing: false,
   actions: {
     addQuestion: function(){
       this.set('isQuestioning', true);
+    },
+    hideForm: function(){
+      this.set('isQuestioning', false);
+    },
+    seeQuestions: function(){
+      this.set('isViewing', true);
+    },
+    hideQuestions: function(){
+      this.set('isViewing', false);
     },
     saveQuestion: function(){
       var newQuestion = this.store.createRecord('question', {
