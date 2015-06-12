46a31c341fa79d886d82208be5bcb93cdc27eb31
Jessica Engel <Jessicaepdx@gmail.com>
2015-06-12 10:59:27 -0700

    will toggle between showing answer form or add answer button, will clear form after each answer is submitted

--- controllers/question.js
+++ controllers/question.js
@@ -1,6 +1,10 @@
 MessageBoard.QuestionController = Ember.ObjectController.extend({
+  isAnswering: false,
   needs: ['question'],
   actions: {
+    addAnswer: function(){
+      this.set('isAnswering', true);
+    },
     saveAnswer: function(){
       var newAnswer = this.store.createRecord('answer', {
         answerName: this.get('answerName'),
@@ -12,6 +16,12 @@ MessageBoard.QuestionController = Ember.ObjectController.extend({
       var question = this.get('controllers.question.model');
       question.get('answers').pushObject(newAnswer);
       question.save();
+      this.set('answerName', "");
+      this.set('answerDescription', "");
+      this.set('answerImage', "");
+
+      this.set('isAnswering', false);
+
     }
   }
 });
